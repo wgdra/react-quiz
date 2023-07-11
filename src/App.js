b45bc8aef133478b7a3d19b1304a3454da784bcb
wgdra <wg.dragon@proton.me>
2023-07-11 15:48:23 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+
+  return (
+    <div className="app-container">
+      hello
+      <button className='btn btn-primary'>Click Me</button>
+    </div>
+  );
+}
+
+export default App;
